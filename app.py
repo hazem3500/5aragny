d31a8274c9ad13216688fcd5a21cc6923d313344
hazem osama ibrahim <hazem3500@gmail.com>
2017-03-20 20:54:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,83 +1,3 @@
-# from __future__ import print_function
-
-# import json
-# from flask import Flask, request
-# import requests
-
-
-# from wit import Wit
-
-
-# CLIENT_ACCESS_TOKEN = '2K7GXJSRTF7B5ZB74PNZBZQPGFYIIRKC'
-# ACCESS_TOKEN = "EAAEiCmoan6YBADgzCXxzLQZC3HninZAFmwjsqDFcJlXR6G8aYI6xZA7ZBfPngY9IP7xlojZCU0InMle1ZCWoP3yD1mpXCNFfdrIhfUl7mfJMHiWlRmvdPIZAErILzMUlG0g3CalLkZANVnKWfYHxxJhZCbRCdXfZBwf822Ij2o8FMIngZDZD"
- 
-	
-	
-
-
-
-
-# app = Flask(__name__)
-
- 
-# #
-# #def send(request, response):
-# #    """
-# #    Sender function
-# #    """
-# #    # We use the fb_id as equal to session_id
-# #    fb_id = request['session_id']
-# #    text = response['text']
-# #    # send message
-# #    reply(fb_id, text)
-# #	
-
-# def reply(user_id, msg):
-#     data = {
-#         "recipient": {"id": user_id},
-#         "message": {"text": msg}
-#     }
-#     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
-#     return resp.content
-
-# @app.route('/', methods=['GET'])
-# def asd():
-# 	return "Hello world"
-	
-# @app.route('/webhook', methods=['GET'])
-# def handle_verification():
-#     return request.args['hub.challenge']
-
-
-# @app.route('/webhook', methods=['POST'])
-# def handle_incoming_messages():
-# 	data = request.json
-# 	if data['object'] == "page":
-# 		for entry in data['entry']:
-# 			pageID = entry['id']
-# 			for msg in entry['messaging']:
-# 				if msg['message']:
-# 					sender = msg['sender']['id']
-# 					message = msg['message']['text']
-
-
-# 					context0 = {}
-# 					res = client.converse(sender, message, context0)
-
-# 					#reply (sender, str(res))
-# 					reply (sender, res ['msg'])
-# 					#reply (sender, res ['quickreplies'])
-# 					return "ok"
-# 				else:
-# 					return "ok"
-# 	return "ok"
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-
 from flask import Flask, request
 import requests
 import json
@@ -110,7 +30,7 @@ def webhook():
       sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
 	  
       res = client.converse(sender, text, {})
-      reply (sender, res ['msg'])
+      reply (sender, res)
 
     except Exception as e:
       print traceback.format_exc() # something went wrong
