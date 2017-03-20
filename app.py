c0496bc0b8d68082307e9e591c3fc532614a6690
hazem osama ibrahim <hazem3500@gmail.com>
2017-03-20 20:26:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,18 @@
-from __future__ import print_function
-
-import json
 from flask import Flask, request
 import requests
+import json
+import traceback
+import random
 
 
 from wit import Wit
 
 
 CLIENT_ACCESS_TOKEN = '2K7GXJSRTF7B5ZB74PNZBZQPGFYIIRKC'
-ACCESS_TOKEN = "EAAD0FIbuQqcBAA3ISZCodyfmTywDUNTk7ulbL3ZB3LkI6ZAPJGQqnR3iaE75JgZBDZAyri50buticTkJYpFTMOsZCkZBsYVq6JN0ZBE6nUu7RImZAZCfQuZAJp6AuxBuAWQmAOHpZCkIvJv5yDHnICN7ZAzLa2P2eQc0XvIWS1A8JJ1zqNgZDZD"
+token = "EAAD0FIbuQqcBAA3ISZCodyfmTywDUNTk7ulbL3ZB3LkI6ZAPJGQqnR3iaE75JgZBDZAyri50buticTkJYpFTMOsZCkZBsYVq6JN0ZBE6nUu7RImZAZCfQuZAJp6AuxBuAWQmAOHpZCkIvJv5yDHnICN7ZAzLa2P2eQc0XvIWS1A8JJ1zqNgZDZD"
  
-	
-	
-
-
-
 
 app = Flask(__name__)
-
- 
-#
-#def send(request, response):
-#    """
-#    Sender function
-#    """
-#    # We use the fb_id as equal to session_id
-#    fb_id = request['session_id']
-#    text = response['text']
-#    # send message
-#    reply(fb_id, text)
-#	
 
 def reply(user_id, msg):
     data = {
@@ -38,38 +20,26 @@ def reply(user_id, msg):
         "message": {"text": msg}
     }
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
-    return resp.content
 
+@app.route('/', methods=['GET', 'POST'])
+def webhook():
+  if request.method == 'POST':
+    try:
+      data = json.loads(request.data)
+      text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
+      sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
+	  
+      res = client.converse(sender, message, {})
+      reply (sender, res ['msg'])
 
-@app.route('/', methods=['GET'])
-def handle_verification():
-    return request.args['hub.challenge']
+    except Exception as e:
+      print traceback.format_exc() # something went wrong
+  elif request.method == 'GET': # For the initial verification
+    return request.args.get('hub.challenge')
 
-
-@app.route('/', methods=['POST'])
-def handle_incoming_messages():
-	data = request.json
-	if data['object'] == "page":
-		for entry in data['entry']:
-			pageID = entry['id']
-			for msg in entry['messaging']:
-				if msg['message']:
-					sender = msg['sender']['id']
-					message = msg['message']['text']
-
-
-					context0 = {}
-					res = client.converse(sender, message, context0)
-
-					#reply (sender, str(res))
-					reply (sender, res ['msg'])
-					#reply (sender, res ['quickreplies'])
-					return "ok"
-				else:
-					return "ok"
-	return "ok"
 
 client = Wit(access_token=CLIENT_ACCESS_TOKEN)
  
+
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
